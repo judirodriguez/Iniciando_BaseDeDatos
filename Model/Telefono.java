4de108ba301670a1b0353feec8cb752ac1f11824
judirodriguez <jurodriguez.88@gmail.com>
2022-11-12 04:27:09 -0300

    iniciando en base de datos

---
+++ Model/Telefono.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Telefono {
+
+    /**
+     * Default constructor
+     */
+    public Telefono() {
+    }
+
+    /**
+     * 
+     */
+    private int numero;
+
+    /**
+     * 
+     */
+    private string cod_pais;
+
+    /**
+     * 
+     */
+    public string compañia;
+
+    /**
+     * 
+     */
+    public string pospago;
+
+    /**
+     * 
+     */
+    public void Operation1() {
+        // TODO implement here
+    }
+
+}
---
+++ Model/Usuario.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Usuario {
+
+    /**
+     * Default constructor
+     */
+    public Usuario() {
+    }
+
+    /**
+     * 
+     */
+    private string calle;
+
+    /**
+     * 
+     */
+    private int numero;
+
+    /**
+     * 
+     */
+    private string sector;
+
+    /**
+     * 
+     */
+    private string region;
+
+    /**
+     * 
+     */
+    private string ciudad;
+
+}
